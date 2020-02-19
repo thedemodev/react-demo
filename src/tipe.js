b6864446fad1f7a24dfbf025451c4e41d35469f2
Scott Moss <willscottmoss@gmail.com>
2020-02-19 08:57:15 -0800

    chore

--- src/tipe.js
+++ src/tipe.js
@@ -1,37 +1,6 @@
 import Markdown from './editors/markdown'
 
 export const schema = {
-  link: {
-    template: 'object',
-    type: 'link',
-    name: 'Link',
-    fields: {
-      url: {
-        type: 'string',
-        name: 'url'
-      },
-      name: {
-        type: 'string',
-        name: 'name'
-      }
-    }
-  },
-  links: {
-    template: 'array',
-    type: 'links',
-    contains: [{type: 'link', name: 'Link'}]
-  },
-  menu: {
-    template: 'document',
-    type: 'menu',
-    name: 'Menu',
-    fields: {
-      links: {
-        type: 'links',
-        name: 'links'
-      }
-    }
-  },
   homePage: {
     template: 'document',
     type: 'homePage',
